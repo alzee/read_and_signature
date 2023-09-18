afc5d975ad9feeba4f9e046a361b541d9aa2bc2c
Al Zee <z@alz.ee>
2023-09-18 17:46:02 +0800

    nth

--- js/main.js
+++ js/main.js
@@ -7,11 +7,36 @@ const capture = () => {
     const dataURL = canvas.toDataURL();
     let a = document.createElement("a");
     a.href = dataURL;
-    a.download = 'test';
+    a.download = fmtDate(new Date(), 'YYMMDDhhmmss');
     a.click();
   });
+
+  cl();
 }
 
 const cl = () => {
   signaturePad.clear();
+}
+
+function fmtDate(time,format='YY-MM-DD hh:mm:ss'){
+  var date = new Date(time);
+
+  var year = date.getFullYear(),
+    month = date.getMonth()+1,
+    day = date.getDate(),
+    hour = date.getHours(),
+    min = date.getMinutes(),
+    sec = date.getSeconds();
+  var preArr = Array.apply(null,Array(10)).map(function(elem, index) {
+    return '0'+index;
+  });
+
+  var newTime = format.replace(/YY/g,year)
+  .replace(/MM/g,preArr[month]||month)
+  .replace(/DD/g,preArr[day]||day)
+  .replace(/hh/g,preArr[hour]||hour)
+  .replace(/mm/g,preArr[min]||min)
+  .replace(/ss/g,preArr[sec]||sec);
+
+  return newTime;			
 }
